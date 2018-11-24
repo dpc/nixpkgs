9ee868fc8e23ca23bf074381776ba8c0c0cae956
Dawid Ciężarkiewicz <dpc@dpc.pw>
2018-11-23 19:51:39 -0800

    Use my own branch

--- pkgs/applications/editors/kakoune/default.nix
+++ pkgs/applications/editors/kakoune/default.nix
@@ -3,13 +3,13 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  name = "kakoune-unstable-${version}";
-  version = "2018.10.27";
+  name = "kakoune-unstable-${version}-dpc";
+  version = "2018.11.23";
   src = fetchFromGitHub {
     repo = "kakoune";
-    owner = "mawww";
-    rev = "v${version}";
-    sha256 = "1w7jmq57h8gxxbzg0n3lgd6cci77xb9mziy6lr8330nzqc85zp9p";
+    owner = "dpc";
+    rev = "71b8ed1600115b14bd2d8e1128ea8725bc725da0";
+    sha256 = "0rpqi3rq16kbba8nl44y6w7f5ki4hwndlzr9mq20j46xc5vxmw88";
   };
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ ncurses asciidoc docbook_xsl libxslt ];
